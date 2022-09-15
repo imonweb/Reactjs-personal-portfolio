de808b85c2c500824912c8590c67f35b62a66f6a
Imon <contact@imonweb.co.uk>
2022-09-15 12:59:42 +0100

    skills

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import { NavBar } from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
+      <Projects />
     </div>
   );
 }
